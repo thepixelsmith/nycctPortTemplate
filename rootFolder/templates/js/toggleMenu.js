7740804d24a9450dde7f533ace8d241a4ba074c0
thepixelsmith <thepixelsmith@gmail.com>
2018-03-04 23:52:38 -0500

    Update
    Added Toggle Menu
    Added About Me Page

--- rootFolder/templates/js/toggleMenu.js
+++ rootFolder/templates/js/toggleMenu.js
@@ -1,12 +1,9 @@
 $(document).ready(function () {
     $('.toggleNav').click(function (e) {
-        //alert("this is clicked.");
-        //$(this).toggleClass('active');
         $('.mainNav').toggle();
         e.preventDefault();
     });
-    $('.mainNav').click(function (e){
-        //alert("the main nav function is acive");
+    $('.mainNav').click(function (e) {
         $('.mainNav').toggle();
     });
 });
